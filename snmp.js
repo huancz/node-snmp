4d1b1636afca96929f19912fee43d2372ad877f2
Petr Běhan <behanp@bluepartners.cz>
2011-07-13 14:30:47 +0200

    bugfix: "cannot find module 'snmp_binding'"

--- snmp.js
+++ snmp.js
@@ -1,6 +1,6 @@
 
 var assert = require('assert');
-var binding = require('snmp_binding');
+var binding = require('./snmp_binding');
 var util = require('util');
 
 var hex = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
